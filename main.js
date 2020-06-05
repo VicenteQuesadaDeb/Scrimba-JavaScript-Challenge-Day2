cab859c0553e6a85ed2e0dc43ff29eb433341b5d
Vicente Quesada Camón <vicentequesadadeb@gmail.com>
2020-06-05 03:30:05 +0200

    main.js fix

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function addTwoDigits(num)  {
     //  write code here.
+    //parse a number to string and create an array with the digits as elements
     const array = num.toString().split("")
-    
+    //parse the numbers (string elements) as int
     const firstNumber = parseInt(array[0])
     const secondNumber = parseInt(array[1])    
-    
+    //sum both numbers
     return firstNumber + secondNumber
 
 }
